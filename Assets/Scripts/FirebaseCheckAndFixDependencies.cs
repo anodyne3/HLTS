654b81534f9b13b19d4bd19a899915cb668ca0d8
anodyne3 <45119011+anodyne3@users.noreply.github.com>
2020-02-17 19:29:38 +0100

    Initial commit

---
+++ Assets/Scripts/FirebaseCheckAndFixDependencies.cs
@@ -0,0 +1,30 @@
+﻿using Firebase;
+using UnityEngine;
+
+public class FirebaseCheckAndFixDependencies : MonoBehaviour
+{
+    public FirebaseApp firebaseApp;
+    public bool firebaseReady;
+    
+    private void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                firebaseApp = FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                firebaseReady = true;
+            }
+            else
+            {
+                Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+}
