fdee80b49febe6a26e058bdf4f11c791773b515a
anodyne3 <45119011+anodyne3@users.noreply.github.com>
2020-03-06 20:16:48 +0100

    that was a bad idea

--- FirebaseFunctions/functions/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const loadFunctions = require('firebase-function-tools');
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const config = functions.config().firebase;
-admin.initializeApp(config);
-loadFunctions(__dirname, exports);
-//# sourceMappingURL=index.js.map
