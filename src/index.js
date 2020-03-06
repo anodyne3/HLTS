948c7e1e5376b520b239715df92f444457af9f69
anodyne3 <45119011+anodyne3@users.noreply.github.com>
2020-03-06 19:14:13 +0100

    still trying

--- src/index.js
+++
@@ -1,6 +0,0 @@
-var loadFunctions = require('firebase-function-tools');
-var admin = require('firebase-admin');
-var functions = require('firebase-functions');
-var config = functions.config().firebase;
-admin.initializeApp(config);
-loadFunctions(__dirname, exports);
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-const loadFunctions = require('firebase-function-tools');
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const config = functions.config().firebase;
-
-admin.initializeApp(config);
-
-loadFunctions(__dirname, exports);
